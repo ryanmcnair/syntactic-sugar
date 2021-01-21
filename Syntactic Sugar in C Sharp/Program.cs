c5516d8bdd901c08e516705216a3e884459e2e38
Ryan McNair <ryan.mcnair@gmail.com>
2021-01-21 17:58:56 -0600

    setup repo for study group exercise

---
+++ Syntactic Sugar in C Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Syntactic_Sugar_in_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
